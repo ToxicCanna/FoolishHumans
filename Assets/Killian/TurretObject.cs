810fd6f9b9c0d48e95db61c1dceb412c07572819
ToxicCanna <freedom22140@gmail.com>
2024-10-21 15:39:31 -0600

    Create Gym and Start Turret Scriptable Object

---
+++ Assets/Killian/TurretObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "TurretData", menuName = "ScriptableObjects/TurretObject", order = 1)]
+public class TurretObject : ScriptableObject
+{
+    public float atkSpd;
+    public float atkDmg;
+    public float atk;
+
+
+}
