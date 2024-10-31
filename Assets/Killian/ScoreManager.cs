b191537e88cc06ecebe0be507f88c8afd561af8c
ToxicCanna <freedom22140@gmail.com>
2024-10-30 20:26:28 -0600

    ScoreManager Fix

--- Assets/Killian/ScoreManager.cs
+++ Assets/Killian/ScoreManager.cs
@@ -1,23 +1,9 @@
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour
+public class ScoreManager : Singleton<ScoreManager>
 {
-    public static ScoreManager Instance;
 
     public int blood; //Player's money
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 
     public void AddBlood(int amount)
     {
